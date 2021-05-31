4dd2b175068bd9a5c1084df7fd7dd7750928e829
Paola Chiappetta <chiappettapaola@gmail.com>
2021-05-31 20:06:17 -0300

    Clase Rotura

---
+++ src/com/Hotel/Rotura.java
@@ -0,0 +1,28 @@
+package com.Hotel;
+
+public class Rotura extends Extra {
+
+    private String causa;
+
+    public Rotura() {
+    }
+
+    public Rotura(String nombre, Double precio, String causa) {
+        super(nombre, precio);
+        this.causa = causa;
+    }
+
+    public String getCausa() {
+        return causa;
+    }
+
+    public void setCausa(String causa) {
+        this.causa = causa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nCausa: " + this.causa;
+    }
+}
