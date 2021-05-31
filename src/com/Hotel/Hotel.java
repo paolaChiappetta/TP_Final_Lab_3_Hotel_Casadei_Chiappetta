1520a72580b82e98741af2180941fd653dc02256
Micaela <micac88@hotmail.com>
2021-05-31 20:53:30 -0300

    Clase Hotel

---
+++ src/com/Hotel/Hotel.java
@@ -0,0 +1,16 @@
+package com.Hotel;
+
+import java.util.List;
+
+public class Hotel {
+
+    private List<Habitacion> listaHabitaciones;
+    private List<Reserva> listaReservas;
+    private List<Recepcionista> recepcionistas;
+    //private List<Ocupacion> listaOcupaciones;
+
+    private Administrador administrador;
+    private Recepcionista recpecionista;
+
+    private Shop shop;
+}
