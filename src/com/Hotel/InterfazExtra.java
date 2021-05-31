ae33df067206b00aa281be87c99fb03f2aace45c
Paola Chiappetta <chiappettapaola@gmail.com>
2021-05-31 20:20:31 -0300

    Interfaz Extra

---
+++ src/com/Hotel/InterfazExtra.java
@@ -0,0 +1,8 @@
+package com.Hotel;
+
+public interface InterfazExtra {
+    Extra nuevoExtra();
+    void eliminarExtra (String nombre);
+    void bajaExtra (String nombre);
+    void modificarExtra (String nombre);
+}
