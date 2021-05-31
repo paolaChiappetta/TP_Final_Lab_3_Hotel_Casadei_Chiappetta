cc8b41a841addf304658bacd66811897e2a6633d
Micaela <micac88@hotmail.com>
2021-05-31 18:18:58 -0300

    Clase Persona

---
+++ src/com/Hotel/Persona.java
@@ -0,0 +1,34 @@
+package com.Hotel;
+
+import java.time.LocalDate;
+
+public abstract class Persona {
+
+    private String nombre;
+    private String apellido;
+
+
+    public Persona() {}
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {  return nombre;    }
+
+    public void setNombre(String nombre) {   this.nombre = nombre;  }
+
+    public String getApellido() {    return apellido;  }
+
+    public void setApellido(String apellido) {    this.apellido = apellido;  }
+
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre +
+                "\nApellido: " + apellido;
+    }
+
+
+}
