43c94242e5f1e3fc955e8d08fa78322ab43e5587
Paola Chiappetta <chiappettapaola@gmail.com>
2021-05-31 20:06:39 -0300

    Clase Producto Minibar

---
+++ src/com/Hotel/ProductoMinibar.java
@@ -0,0 +1,29 @@
+package com.Hotel;
+
+public class ProductoMinibar extends Extra {
+    
+    private String marca;
+
+    public ProductoMinibar() {
+    }
+
+    public ProductoMinibar(String nombre, Double precio, String marca) {
+        super(nombre, precio);
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nMarca: " + this.marca;
+    }
+
+}
