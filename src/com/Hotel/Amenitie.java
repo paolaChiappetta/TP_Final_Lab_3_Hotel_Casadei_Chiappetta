44ccd26798d729302a31d366d829ce855aae2cd9
Paola Chiappetta <chiappettapaola@gmail.com>
2021-05-31 20:06:57 -0300

    Clase Amenitie

---
+++ src/com/Hotel/Amenitie.java
@@ -0,0 +1,39 @@
+package com.Hotel;
+
+public class Amenitie extends Extra {
+
+    private String descripcion;
+    private String horario;
+
+    public Amenitie() {
+    }
+
+    public Amenitie(String nombre, Double precio, String descripcion, String horario) {
+        super(nombre, precio);
+        this.descripcion = descripcion;
+        this.horario = horario;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nDescripción: " + this.descripcion +
+                "\nHorario: " + this.horario;
+    }
+}
