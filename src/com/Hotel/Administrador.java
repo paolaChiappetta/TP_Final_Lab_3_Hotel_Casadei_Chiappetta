8464e74a593c27d93fe620184f07ef791e4a6d32
Micaela <micac88@hotmail.com>
2021-06-08 23:57:23 -0300

    Clase Admin- modificar empleado

--- src/com/Hotel/Administrador.java
+++ src/com/Hotel/Administrador.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class Administrador extends Empleado implements InterfazAccion, Serializable {
 
     //constructor vacío
-    public Administrador() { }
+    public Administrador() {
+    }
 
     //constructor con datos p/ generación de usuario y contraseña
     public Administrador(String nombre, String apellido, String numeroTel, String dni) {
@@ -22,25 +23,28 @@ public class Administrador extends Empleado implements InterfazAccion, Serializa
 
     ///Override de la interfazAccion
 
-    @Override
+
+@Override
     public void menuModificarHabitacion() {
 
-        System.out.println("1: Numero");
+        System.out.println("1: Numero habitacion");
         System.out.println("2: Piso");
-        System.out.println("3: Estado");
-        System.out.println("4: Tarifa");
-        System.out.println("5: Fecha proxima ocupacion");
+        System.out.println("3: Tipo de Habitacion- Tarifa");
+        System.out.println("4: Estado de habitacion");
 
-        System.out.println("0 para Finalizar");
+        System.out.println("\n0 para finalizar");
 
     }
 
+    //MODIFICAR UNA HABITACION, la recibe por parametro
     @Override
     public void modificarHabitacion(Habitacion habitacion) {
 
-        System.out.println("Indique el dato que desea modificar: ");
-        int opcion = 0;
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Indique que datos desea modificar: ");
+        int opcion = 0;
+
 
         do {
             menuModificarHabitacion();
@@ -50,65 +54,75 @@ public class Administrador extends Empleado implements InterfazAccion, Serializa
                     System.out.println("Numero:");
                     habitacion.setNumero(scanner.nextInt());
                     break;
-
                 case 2:
                     System.out.println("Piso:");
                     habitacion.setPiso(scanner.nextInt());
                     break;
                 case 3:
-                    System.out.println("Estado:");
-                    habitacion.indicarEstadoHabitacion();
-                    break;
-
-                case 4:
                     System.out.println("Tarifa:");
                     habitacion.indicarTipoHabitacion();
                     break;
-
-                case 5:
-                    System.out.println("Fecha de proxima ocupacion:");
-                    habitacion.getFechaProximaOcupacion(LocalDate.parse(scanner.next(), DateTimeFormatter.BASIC_ISO_DATE));
+                case 4:
+                    System.out.println("Estado:");
+                    habitacion.indicarEstadoHabitacion();
                     break;
 
                 default:
                     System.out.println("Opcion incorrecta, ingrese nuevamente");
                     break;
-
             }
-
         } while (opcion != 0);
-
     }
 
-/*
-    public Empleado generarNuevoEmpleado (){
+
+
+
+    public void menuModificarEmpleado() {
+        System.out.println("1: Nombre");
+        System.out.println("2: Apellido");
+        System.out.println("3: Dni");
+        System.out.println("4: Telefono");
+
+
+        System.out.println("\n0 para finalizar");
+    }
+
+
+    public void modificarEmpleado(Empleado empleado) {
         Scanner scanner = new Scanner(System.in);
-        String nombre, apellido, telefono;
-        int opcion;
 
-        System.out.println("Nombre:");
-        nombre= scanner.nextLine();
-        System.out.println("Apellio:");
-        apellido= scanner.nextLine();
-        System.out.println("Telefono:");
-        telefono= scanner.nextLine();
+        System.out.println("Indique que datos desea modificar: ");
+        int opcion = 0;
 
-        System.out.println("Indique:  1= Recepcionista   2= Administrador");
-        opcion= scanner.nextInt();
 
-        if(opcion== 1){
+        do {
+            menuModificarEmpleado();
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Nombre:");
+                    empleado.setNombre(scanner.nextLine());
+                    break;
+                case 2:
+                    System.out.println("Apellido:");
+                    empleado.setApellido(scanner.nextLine());
+                    break;
+                case 3:
+                    System.out.println("Dni:");
+                    empleado.setDni(scanner.nextLine());
+                    break;
+                case 4:
+                    System.out.println("Telefono:");
+                    empleado.setNumeroTel(scanner.nextLine());
+                    break;
 
-            Recepcionista recepcionista= new Recepcionista(nombre, apellido, telefono);
 
-            return recepcionista;
-        }else{
-
-            Administrador administrador= new Administrador(nombre, apellido, telefono);
-
-            return administrador;
-        }
-
-    }*/
+                default:
+                    System.out.println("Opcion incorrecta, ingrese nuevamente");
+                    break;
+            }
+        } while (opcion != 0);
+    }
 
 }
 
